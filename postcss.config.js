61d9da2f3180bc25b6081a70e7ddf61ac4bd1efe
Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2021-08-11 13:55:35 +0800

    Re-initial commit

---
+++ postcss.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const fontFamily = require('./util/fontFamily');
+const postcssSorting = require('./util/postcssSorting');
+
+module.exports = {
+  plugins: {
+    autoprefixer: {},
+    '@fullhuman/postcss-purgecss': process.env.NODE_ENV === 'production' && {
+      content: [path.join(__dirname, '**/*.html'), path.join(__dirname, '**/*.js')],
+      defaultExtractor: (content) => {
+        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]+/g) || [];
+        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]+/g) || [];
+        return broadMatches.concat(innerMatches);
+      },
+    },
+    'postcss-font-family-system-ui': {
+      family: [...fontFamily.sans],
+      preserve: false,
+    },
+    'postcss-import': {},
+    'postcss-sort-media-queries': {},
+    'postcss-sorting': {
+      order: [...postcssSorting.order],
+      'properties-order': [...postcssSorting.propertiesOrder],
+      'unspecified-properties-position': 'bottom',
+    },
+  },
+};
