bb9aacb1a3b0cda6f3dd5cf1541c71ebcb3c64bd
Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2021-08-14 22:18:37 +0800

    Remove `cssnano` plugin

--- postcss.config.js
+++ postcss.config.js
@@ -14,10 +14,6 @@ module.exports = {
       },
       blocklist: ['summary'],
     },
-    cssnano: {
-      preset: 'default',
-      preferredQuote: 'single',
-    },
     'postcss-font-family-system-ui': {
       family: [...fontFamily.sans],
       preserve: false,
